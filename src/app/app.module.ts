4ed4ba7449ab8a2e7e1942eaeff0f74bd380eeab
suzukikarol <karol.suzuki.23@gmail.com>
2020-04-08 09:32:45 -0300

    adicionando component ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversorComponent } from './conversor/conversor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorComponent
   ],
   imports: [
     BrowserModule,
